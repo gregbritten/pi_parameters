03acd730a5832e4e442d1ebcfab2224ac299b7ee
Gregory Britten <gregleebritten@gmail.com>
2023-06-19 13:30:38 -0400

    cleaning up figures

--- R/fig_map.R
+++ R/fig_map.R
@@ -1,4 +1,5 @@
 library(maps)
+library(viridis)
 
 source('r/process_csvs.r') ##--data processing--###########
 
@@ -8,18 +9,18 @@ d     <- as.data.frame(D[[1]])
 d$lat <- jitter(d$lat,factor=100)
 d$lon <- jitter(d$lon,factor=100)
 
+regions <- c('scot','lab','spac','tas','ice')
+cols <- turbo(5)
 
-pdf('~/dropbox/working/pi_parameters/github/plots/map_obs.pdf',height=5,width=7.5)
-par(oma=c(2,2,2,2))
+pdf('~/dropbox/working/pi_parameters/github/plots/map_obs.pdf',height=4.5,width=6.5)
+par(mfrow=c(1,1),oma=c(2,2,2,2))
 map(ylim=c(-90,90))
   box(lwd=2)
   #points(jitter(d$lon,factor=100),jitter(d$lat,factor=100),pch=19,cex=0.2,col='red')
-  points(d$lon,d$lat,pch=19,cex=0.2)
-  points(d$lon[d$region=='spac'],d$lat[d$region=='spac'],pch=19,cex=0.2,col='red')
-  points(d$lon[d$region=='tas'],d$lat[d$region=='tas'],pch=19,cex=0.2,col='purple')
-  points(d$lon[d$region=='scot'],d$lat[d$region=='scot'],pch=19,cex=0.2,col='orange')
-  points(d$lon[d$region=='lab'],d$lat[d$region=='lab'],pch=19,cex=0.2,col='blue')
-  points(d$lon[d$region=='ice'],d$lat[d$region=='ice'],pch=19,cex=0.2,col='dark green')
+    points(d$lon[!(d$region%in%regions)],d$lat[!(d$region%in%regions)],pch=4,cex=0.2,col='dark grey')
+  for(i in 1:length(regions)){
+    points(d$lon[d$region==regions[i]],d$lat[d$region==regions[i]],pch=4,cex=0.3,col=cols[i],lwd=0.5)
+  }
   abline(h=seq(-90,90,10),lty=2,lwd=0.25)
   abline(v=seq(-180,180,10),lty=2,lwd=0.25)
   axis(side=2,at=seq(-90,90,10),cex.axis=0.6,las=2)
--- R/process_csvs.R
+++ R/process_csvs.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
 
-dir <- "~/dropbox/working/pi_parameters/mapps_forest/upstream_files" #github repo
+dir <- "~/dropbox/working/pi_parameters/mapps_forest/upstream_files/statistics/" #github repo
 
 mean_files <- file.path(dir,list.files(dir)[grep('mean',list.files(dir))])
 sd_files   <- file.path(dir,list.files(dir)[grep('std',list.files(dir))])
